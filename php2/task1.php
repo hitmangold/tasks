abb3d44b392d5adde38fbb3dbc69b45fb34a0c43
hitmangold <samsonyan.it@gmail.com>
2022-10-04 14:28:51 +0400

    PHP tasks1

--- php2/task1.php
+++ php2/task1.php
@@ -11,37 +11,9 @@ function randint_arr($arr,$count){
     }
     return $arr;
 }
-function bubble_Sort($my_array,$order)
-{
-    do
-    {
-        $swapped = false;
-        for( $i = 0, $c = count( $my_array ) - 1; $i < $c; $i++ )
-        {
-            if($order == 'DESC'){
-                if( $my_array[$i] < $my_array[$i + 1] )
-                {
-                    list( $my_array[$i + 1], $my_array[$i] ) =
-                        array( $my_array[$i], $my_array[$i + 1] );
-                    $swapped = true;
-                }
-            }
-            else if($order == 'ASC'){
-                if( $my_array[$i] > $my_array[$i + 1] )
-                {
-                    list( $my_array[$i + 1], $my_array[$i] ) =
-                        array( $my_array[$i], $my_array[$i + 1] );
-                    $swapped = true;
-                }
-            }
-        }
-    }
-    while( $swapped );
-    return $my_array;
-}
 $arr = randint_arr($arr,50);
 print_r($arr);
-$arr = bubble_Sort($arr,'DESC');
+rsort($arr);
 $arr = array_unique($arr);
 $arr = array_values($arr);
 echo '<table style="width: 250px; border: 1px solid black; margin-top: 30px;">';
